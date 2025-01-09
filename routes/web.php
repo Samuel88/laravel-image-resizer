afdcae7b91e175d19a8a0faf20a942687b25eca3
Samuel Panicucci <samuel.panicucci@gmail.com>
2025-01-09 18:24:46 +0100

    Add Product Model

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,4 @@ Route::get('/', function () {
 });
 
 Route::resource('pages',PageController::class);
+Route::resource('products', ProductController::class);
